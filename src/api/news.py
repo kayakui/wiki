ff12050ddc1d6036a8be4dc4ef2c82380c3f1470
kayakui <tectrix353@gmail.com>
2025-07-17 14:57:22 -0500

    api fix

--- src/api/news.py
+++ src/api/news.py
@@ -8,28 +8,9 @@ from bs4 import BeautifulSoup as bs
 load_dotenv()
 
 news_id = os.getenv("NEWS_ID")
-york_id = os.getenv("NEWYORK_ID")
-
-def get_most_shared_fb_york():
-    url = f"https://api.nytimes.com/svc/mostpopular/v2/shared/1/facebook.json?api-key={york_id}"
-    result = get(url)
-    json_result = json.loads(result.content)
-    return json_result
-
-def sort_titles_york():
-    print("Titles:")
-
-    for idx, title in enumerate(shared['results']):
-        print(f"{idx + 1}. {shared['results'][idx]['title']}")
-
-def get_abstracts_york():
-    print("Descriptions:")
-
-    for idx, abstracts in enumerate(shared['results']):
-        print(f"{idx + 1}. {shared['results'][idx]['abstract']}")
 
 def get_article_news():
-    url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={news_id}"
+    url = f"https://newsapi.org/v2/top-headlines?sources=cnn&apiKey={news_id}"
     result = get(url)
     json_result = json.loads(result.content)
     return json_result
@@ -47,19 +28,40 @@ def get_url():
 
     return links
 
-shared = get_most_shared_fb_york()
+
 article = get_article_news()
 url = get_url()
 
-# num = 6
-#
+def get_html():
+    with urlopen(url[4]) as responce:
+        content = responce.read()
+
+    soup = bs(content, 'html.parser')
+    article_body = soup.find(class_='article__content')
+    # text = [p.find(string=True) for p in soup.find_all('p')]
+
+    par = []
+    data = []
+
+    for i in article_body:
+        par.append(article_body.get_text(strip=True))
+        # data.append(par)
+        # print(par)
+
+
+    return article_body
+
+
+# def format_html():
+#     if
 # def get_html():
-#     with urlopen(url[num]) as responce:
+#     with urlopen(url[3]) as responce:
 #         content = responce.read()
 #
 #     soup = bs(content, 'html.parser')
-#     article_body = soup.find_all(class_='article-body__content')
-#     text = [p.find(string=True, recursive=False) for p in soup.find_all('p')]
+#     article_body = soup.find_all(class_='article')
+#     if article_body:
+#         text = [p.find(string=True, recursive=False) for p in soup.find_all('p')]
 #     par = []
 #
 #     for i, para in enumerate(text):
@@ -68,7 +70,7 @@ url = get_url()
 #     # print(par)
 #     return par
 #
-# article_text = get_html()
+article_text = get_html()
 #
 # def convert_json():
 #     data = []
@@ -86,63 +88,59 @@ url = get_url()
 #
 #
 
-def full_creation():
-    num = 0
-    full = []
-    for i in range(len(url)):
-        try:
-            with urlopen(url[num]) as responce:
-                content = responce.read()
-
-            soup = bs(content, 'html.parser')
-            article_body = soup.find_all(class_='article-body__content')
-            text = [p.find(string=True, recursive=False) for p in soup.find_all('p')]
-            par = []
-
-            for i, para in enumerate(text):
-                par.append(f"{para}")
-
-            json_conv = []
-            for i, text in enumerate(par):
-                key = f"p{i+1}"
-                json_conv.append(key : text)
-
-                full.append(json_conv)
-                num += 1
-
-
-
-        except Exception:
-            num += 1
-            # print(f"{num}DUMASS")
-            pass
-
-        count = 0
-
-        data = {
-                f"text{count + 1}" : full[count]
-                }
-
-        return data
-
-converted = full_creation()
+# def full_creation():
+#     num = 0
+#     for i in range(len(url)):
+#         try:
+#             with urlopen(url[num]) as responce:
+#                 content = responce.read()
+#
+#             soup = bs(content, 'html.parser')
+#             article_body = soup.find_all(class_='article-body__content')
+#             text = [p.find(string=True, recursive=False) for p in soup.find_all('p')]
+#             par = []
+#
+#             for i, para in enumerate(text):
+#                 par.append(f"{para}")
+#
+#             json_conv = []
+#             for i, text in enumerate(par):
+#                 key = f"p{i+1}"
+#                 json_conv.append({key: text})
+#
+#             data = {}
+#             for i, text in enumerate(json_conv):
+#                 data.update({
+#                         f"text{count + 1}" : f"{json_conv[count]}"
+#                         })
+#
+#                 num += 1
+#
 
 
+        # except Exception:
+        #     num += 1
+        #     # print(f"{num}DUMASS")
+        #     pass
+        #
+        # return json_conv
 
-def create_json():
-        json_filename = f"Artircle2.json"
-        json_path = f"/home/kayaki/web/wiki/src/json/{json_filename}"
-
-        with open(json_path, "w") as json_file:
-            json.dump(data, json_file, indent=4)
-
-        print(f"File written to: {json_path}")
+# data = full_creation()
+#
+# def create_json():
+#         json_filename = f"Artircle3.json"
+#         json_path = f"/home/kayaki/web/wiki/src/json/{json_filename}"
+#
+#         with open(json_path, "w") as json_file:
+#             json.dump(data, json_file, indent=4)
+#
+#         print(f"File written to: {json_path}")
 
 
 def main():
-    full_creation()
+    # full_creation()
     # create_json()
-    # print(converted)
+    print(article_text)
 
 
 
