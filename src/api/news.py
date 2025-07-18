dce2fe90e5014345ee3839364fef8862cb162368
kayakui <tectrix353@gmail.com>
2025-07-17 22:25:06 -0500

    api fix2

--- src/api/news.py
+++ src/api/news.py
@@ -33,114 +33,58 @@ article = get_article_news()
 url = get_url()
 
 def get_html():
-    with urlopen(url[4]) as responce:
+    with urlopen(url[0]) as responce:
         content = responce.read()
 
     soup = bs(content, 'html.parser')
     article_body = soup.find(class_='article__content')
-    # text = [p.find(string=True) for p in soup.find_all('p')]
+    text = [p.find(string=True) for p in soup.find_all('p')]
 
     par = []
+    for p in text:
+        par.append(p.get_text(strip=True))
+
+    p = filter(None, par)
     data = []
 
-    for i in article_body:
-        par.append(article_body.get_text(strip=True))
-        # data.append(par)
-        # print(par)
+    for x in p:
+        data.append(x)
 
+    return data
 
-    return article_body
-
-
-# def format_html():
-#     if
-# def get_html():
-#     with urlopen(url[3]) as responce:
-#         content = responce.read()
-#
-#     soup = bs(content, 'html.parser')
-#     article_body = soup.find_all(class_='article')
-#     if article_body:
-#         text = [p.find(string=True, recursive=False) for p in soup.find_all('p')]
-#     par = []
-#
-#     for i, para in enumerate(text):
-#         par.append(f"{para}")
-#
-#     # print(par)
-#     return par
-#
 article_text = get_html()
-#
-# def convert_json():
-#     data = []
-#     for i, text in enumerate(article_text):
-#         key = f"p{i+1}"
-#         data.append({key : text})
-#
-#     return data
-#
-# convert = convert_json()
-#
-# data = {
-#         "text1" : convert
-#     }
-#
-#
+
+def convert_json():
+    data = []
+    i = 0
+    for par in article_text:
+        key = f"{i + 1}"
+        data.append({f"{key}" : f"{par}"})
+        i += 1
+
+    return data
+
+convert = convert_json()
+
+data = {
+        "text" : convert
+    }
 
 # def full_creation():
-#     num = 0
 #     for i in range(len(url)):
-#         try:
-#             with urlopen(url[num]) as responce:
-#                 content = responce.read()
-#
-#             soup = bs(content, 'html.parser')
-#             article_body = soup.find_all(class_='article-body__content')
-#             text = [p.find(string=True, recursive=False) for p in soup.find_all('p')]
-#             par = []
-#
-#             for i, para in enumerate(text):
-#                 par.append(f"{para}")
-#
-#             json_conv = []
-#             for i, text in enumerate(par):
-#                 key = f"p{i+1}"
-#                 json_conv.append({key: text})
-#
-#             data = {}
-#             for i, text in enumerate(json_conv):
-#                 data.update({
-#                         f"text{count + 1}" : f"{json_conv[count]}"
-#                         })
-#
-#                 num += 1
 #
 
+def create_json():
+        json_filename = "Artircles.json"
+        json_path = f"/home/kayaki/web/wiki/src/json/{json_filename}"
 
-        # except Exception:
-        #     num += 1
-        #     # print(f"{num}DUMASS")
-        #     pass
-        #
-        # return json_conv
+        with open(json_path, "w") as json_file:
+            json.dump(data, json_file, indent=4)
 
-# data = full_creation()
-#
-# def create_json():
-#         json_filename = f"Artircle3.json"
-#         json_path = f"/home/kayaki/web/wiki/src/json/{json_filename}"
-#
-#         with open(json_path, "w") as json_file:
-#             json.dump(data, json_file, indent=4)
-#
-#         print(f"File written to: {json_path}")
-
+        print(f"File written to: {json_path}")
 
 def main():
-    # full_creation()
-    # create_json()
-    print(article_text)
+    full_creation()
 
 
 
